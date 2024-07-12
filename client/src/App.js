78e3c960830f09a0d1498bfb870731f438044aee
salman karim <124361112+Salman-Khan9@users.noreply.github.com>
2024-07-12 18:48:52 +0500

    house page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
-import Deals from './Pages/Deals/Deals';
-import Login from './Pages/Login/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Deals from "./Pages/Deals/Deals";
+import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import Hotel from "./Pages/Hotels/Hotel";
 import TripPage from "./Pages/TripPage/TripPage";
+import House from "./Pages/House/House";
 function App() {
   return (
     <>
       <Router>
         <Routes>
-        <Route path='/' element = {<Hotel/>}/>
-          <Route path='/Deals' element = {<Deals/>}/>
-          <Route path='/trip' element = {<TripPage/>}/>
-          <Route path='/login' element = {<Login/>}/>
-          <Route path='/signup' element = {<Signup/>}/>
+          <Route path="/" element={<Hotel />} />
+          <Route path="/Deals" element={<Deals />} />
+          <Route path="/trip" element={<TripPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/house" element={<House />} />
         </Routes>
       </Router>
     </>
